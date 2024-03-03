8141859a4311ade66355db02880262b228789a05
Yeifer Muñoz <developer@yeifer.co>
2024-03-02 21:09:00 -0500

    feat(task): :truck: Add task router to app

--- src/config/router.js
+++ src/config/router.js
@@ -1,5 +1,7 @@
 import indexRouter from '@/routes/index';
+import taskRouter from '@/routes/task';
 
 export default function (app) {
   app.use('/', indexRouter);
+  app.use('/task(s)?', taskRouter);
 }
